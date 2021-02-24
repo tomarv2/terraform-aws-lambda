b6c904870f9c2e26e9978fdee3b0d992fcc7ba1a
tomarv2 <varuntomar2019@gmail.com>
2021-02-24 07:43:38 -0800

    moving version to seperate file

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-//  required_version            = ">= 0.14"
-  required_providers {
-    aws = {
-//      version                     = "~> 3.0"
-    }
-  }
-}
-
-provider "aws" {
-  region                      = var.aws_region
-  profile                     = var.profile_to_use
-}
-
 locals {
   shared_tags  = map(
       "Name", "${var.teamid}-${var.prjid}",
---
+++ version.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version            = ">= 0.14"
+  required_providers {
+    aws = {
+      version                     = "3.29"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = var.aws_region
+  profile                     = var.profile_to_use
+}
