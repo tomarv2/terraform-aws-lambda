825b86061a7aba9033a69714c6d4048d551a421e
tomarv2 <varuntomar2019@gmail.com>
2021-03-29 23:31:49 -0700

    fixing test

--- tests/unit_test.go
+++ tests/unit_test.go
@@ -1,21 +1,18 @@
 package test
 
 import (
-// 	"fmt"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/aws"
-// 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/require"
 )
 
-// An example of how to test the Terraform module in examples/terraform-aws-lambda-example using Terratest.
+// Test Terraform module using Terratest.
 func TestTerraformAwsLambda(t *testing.T) {
 	t.Parallel()
 	// ----------------------------------------------------------
-// 	TF_VARS_FILE_PATH       := "test.tfvars"
+    // TF_VARS_FILE_PATH       := "test.tfvars"
 	TF_REPO_PATH            := "../examples"
 	functionName            := "rumse-demo-lambda"
 	teamid                  := "rumse"
@@ -26,9 +23,8 @@ func TestTerraformAwsLambda(t *testing.T) {
 	// terraform testing.
 	terraformOptions := &terraform.Options{
 		TerraformDir: TF_REPO_PATH,
-
-// 		// Variables to pass to our Terraform code using -var-file options
-// 		VarFiles: []string{TF_VARS_FILE_PATH},
+ 		// Variables to pass to our Terraform code using -var-file options
+ 		// VarFiles: []string{TF_VARS_FILE_PATH},
 
 		// Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
@@ -45,24 +41,13 @@ func TestTerraformAwsLambda(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	// Invoke the function, so we can test its output
-	response := aws.InvokeFunction(t, awsRegion, functionName, ExampleFunctionPayload{ShouldFail: false, Echo: "hi!"})
+	response := aws.InvokeFunction(t, awsRegion, functionName, LambdaFunctionPayload{ShouldFail: false, Echo: "hi!"})
 
 	// This function just echos it's input as a JSON string when `ShouldFail` is `false``
 	assert.Equal(t, `"hi!"`, string(response))
-
-    // TODO: need to spend sometime here
-	// Invoke the function, this time causing it to error and capturing the error
-	//response, err := aws.InvokeFunctionE(t, awsRegion, functionName, ExampleFunctionPayload{ShouldFail: true, Echo: "hi!"})
-
-    // 	// Function-specific errors have their own special return
-    // 	functionError, ok := err.(*aws.FunctionError)
-    // 	require.True(t, ok)
-    //
-    // 	// Make sure the function-specific error comes back
-    // 	assert.Contains(t, string(functionError.Payload), "Failed to handle")
 }
 
-type ExampleFunctionPayload struct {
+type LambdaFunctionPayload struct {
 	Echo       string
 	ShouldFail bool
 }
