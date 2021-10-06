81205e600f3b030ccd10869738f4956a40056baf
tomarv2 <varuntomar2019@gmail.com>
2021-10-06 14:20:32 -0700

    adding function name to output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "lambda_arn" {
   value       = join("", aws_lambda_function.lambda.*.arn)
 }
 
+output "lambda_function_name" {
+  description = "Name of the Lambda function"
+  value       = join("", aws_lambda_function.lambda.*.function_name)
+}
+
 output "lambda_iam_role_arn" {
   description = "ARN of IAM role used by Lambda function"
   value       = module.iam_role.iam_role_arn
