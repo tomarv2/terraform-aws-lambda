f1051a7f4620654551f02998bdcd7ba39ae3208c
tomarv2 <varuntomar2019@gmail.com>
2022-10-20 04:50:16 +0000

    Sync from management repo

--- examples/lambda_with_vpc/outputs.tf
+++ examples/lambda_with_vpc/outputs.tf
@@ -1,34 +1,24 @@
 output "lambda_arn" {
   description = "ARN of the Lambda function"
-  value       = module.lambda.lambda_arn
+  value       = module.lambda.arn
 }
 
-output "input_file_name" {
+output "source_file" {
+  description = "Lambda source code location"
+  value       = module.lambda.source_file
+}
+
+output "output_path" {
+  description = "Output file path location"
+  value       = module.lambda.output_path
+}
+
+output "output_size" {
+  description = "Output file size"
+  value       = module.lambda.output_size
+}
+
+output "source_dir" {
   description = "Source code location"
-  value       = module.lambda.input_file_name
-}
-
-output "output_file_path" {
-  description = "Output filepath location"
-  value       = module.lambda.output_file_path
-}
-
-output "output_file_size" {
-  description = "Output filepath size"
-  value       = module.lambda.output_file_size
-}
-
-output "input_dir_name" {
-  description = "Source code location"
-  value       = module.lambda.input_dir_name
-}
-
-output "output_dir_path" {
-  description = "Output filepath location"
-  value       = module.lambda.output_dir_path
-}
-
-output "output_dir_size" {
-  description = "Output filepath size"
-  value       = module.lambda.output_dir_size
+  value       = module.lambda.source_dir
 }
